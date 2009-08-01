24dce52d401a597d829de1d130e39d4e73e507f0
Samuel A. Falvo II <kc5tja@arrl.net>
2009-08-01 16:03:30 -0700

    code documentation

--- response.fs
+++ response.fs
@@ -1,19 +1,3 @@
-\ PRECONDITIONS:
-\   VARIABLE s -- points to beginning of source string buffer
-\   VARIABLE end -- points just past last character of source buffer
-\   Any template handlers must be defined prior to including this
-\   code.
-\
-\ Builds a template expanded version of the contents of the source
-\ buffer at HERE.  Template expansions start with the tilde character.
-\ Template expansion names map directly to Forth word names.
-\
-\ POSTCONDITIONS:
-\   VARIABLE response -- pointer to first character of response buffer
-\   HERE -- points just past last character of response buffer
-\   Contents of response buffer dumped to stdout for the web server.
-
-: >string     over + end ! s ! ;
 : c           s @ c@ ;
 : -eos        s @ end @ < ;
 : -eon        c 32 > -eos and ;
