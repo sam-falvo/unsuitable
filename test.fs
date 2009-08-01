4be24fb1019e9be5cc1f91c1301723573ac3ec37
Samuel A. Falvo II <kc5tja@arrl.net>
2009-07-31 23:30:18 -0700

    project kickoff for blog software written in Forth.
    What have I gotten myself into?

---
+++ test.fs
@@ -0,0 +1,15 @@
+#! /usr/bin/env gforth
+
+." Content-type: text/html" cr cr
+.\" <html><head><title>HI!</title></head><body><table width=\"100%\"><tr><th>ENV VAR NAME</th><th>ENV VAR VALUE</th></tr>"
+: r ." <tr><td>" type ." </td><td>" type ." </td></tr>" ;
+S" REQUEST_METHOD" getenv S" REQUEST_METHOD" r
+S" REMOTE_ADDR" getenv S" REMOTE_ADDR" r
+S" SCRIPT_NAME" getenv S" SCRIPT_NAME" r
+S" PATH_INFO" getenv S" PATH_INFO" r
+S" SERVER_PROTOCOL" getenv S" SERVER_PROTOCOL" r
+S" QUERY_STRING" getenv S" QUERY_STRING" r
+." </table></body></html>"
+
+bye
+
