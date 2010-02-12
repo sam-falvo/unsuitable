acc7018e42bf8f1d944b19f559d08262affe1fee
Samuel A. Falvo II <kc5tja@arrl.net>
2010-02-11 22:38:24 -0800

    Moved the GOS and resized it to 256KiB of space.  Adjusted all existing pointers into the GOS.

--- mappings.fs
+++ mappings.fs
@@ -7,20 +7,22 @@
 : g-fencepost   1 block ;
 : a-nextId      1 block [ 1 cells ] literal + ;
 
-( blocks 2 .. 65 = general object store )
-2 blocks constant gorg
-gorg 64 blocks + constant gend
+( blocks 2 .. 65 = 64K unused space -- formerly general object store )
 
 ( blocks 66 .. 67 = handle table for general object store )
 1 blocks constant /hfields
 66 blocks constant addrs
 addrs /hfields + constant lens
 
-( blocks 68 .. = article table columns )
+( blocks 68 .. 77 = article table columns )
 2 blocks constant /afields
 68 blocks constant articleIds
 articleIds /afields + constant titles
 titles /afields + constant leads
 leads /afields + constant bodies
 bodies /afields + constant timestamps
+
+( blocks 256 .. 511 = General Object Store [256K version] )
+256 blocks constant gorg
+gorg 256 blocks + constant gend
 
