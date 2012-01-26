b409f96d0f94c344637bd71e3df627207fc7ed3c
Samuel A. Falvo II <kc5tja@arrl.net>
2012-01-25 22:43:42 -0800

    Initial import for Unsuitable 2A.

---
+++ share/unsuitable2/blog.fs
@@ -0,0 +1,9 @@
+.( Content-Type: text/html) cr cr
+.( <html><head><title>Blog Title Here</title>)
+.( <link rel="stylesheet" href="/blog3/theme/css.css" />)
+.( </head><body>)
+.( <p>The blog appears to have been installed correctly; )
+.( however, no articles exist in the message database.</p>)
+.( </body></html>) cr
+bye
+
