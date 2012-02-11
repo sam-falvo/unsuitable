18c6d425a317df2216876efca1c0d88c3490b27a
Samuel A. Falvo II <kc5tja@arrl.net>
2012-02-10 22:21:18 -0800

    Logic to load blobs from GOS storage

--- share/unsuitable2/lib/gos-space.fs
+++ share/unsuitable2/lib/gos-space.fs
@@ -1,7 +1,26 @@
+\ Depends on the following words defined prior:
+\
+\ src ( - a ) Variable that points to the source of a blob.
+\             Depending on the operation performed, the address
+\             may be an xspace pointer or a core pointer.
+\ dst ( - a ) Variable pointing to the destination of a blob.  This
+\             is only used by retrieve at present, and thus is
+\             always a core pointer.
+\ len ( - a ) Variable containing the length of the blob to transfer.
+\ 
+\ gosWofs@ ( - n ) returns the current GOS space write pointer offset.
+\ gosWofs! ( n - ) updates the current GOS space write pointer offset.
+\ core ( xa - ca ) converts an xspace address into a core address.
+
 : reserve   gosWofs@ + gosWofs! ;
 
 : ct        1024 gosWofs@ 1023 and -  len @  min ;
 : advanced  ct >r  r@ src +!  r@ reserve  r> negate len +! ;
-: copy      src @ ct gosWofs@ core swap move update advanced ;
-: keep      begin len @ while copy repeat ;
+: out       src @ ct gosWofs@ core swap move update advanced ;
+: keep      begin len @ while out repeat ;
+
+: ct        1024 src @ 1023 and -  len @ min ;
+: advanced  ct >r  r@ src +!  r@ dst +!  r> negate len +! ;
+: inp       src @ core dst @ ct move advanced ;
+: retrieve  begin len @ while inp repeat ;
 
--- share/unsuitable2/test/gos-space.fs
+++ share/unsuitable2/test/gos-space.fs
@@ -14,6 +14,7 @@ include ../lib/xspace.fs
 include ../lib/metadata.fs
 
 variable src
+variable dst ( unused in this test )
 variable len
 
 include ../lib/gos-space.fs
@@ -49,7 +50,7 @@ variable len
 
 include ../lib/gos-space.fs
 
-: s       srcbuf src !  100 dst !  1024 len !  copy ;
+: s       srcbuf src !  100 dst !  1024 len !  out ;
 : t40.0   s src @ srcbuf - 924 xor abort" t40.0" ;
 : t40.1   s len @ 100 xor abort" t40.1" ;
 : t40.2   s gosWofs@ 1024 xor abort" t40.2" ;
@@ -58,7 +59,7 @@ t40
 
 \ Saving blobs -- aligned chunks
 
-: s       srcbuf src !  0 dst ! 2048 len !  copy ;
+: s       srcbuf src !  0 dst ! 2048 len !  out ;
 : t41.0   s src @ srcbuf - 1024 xor abort" t41.0" ;
 : t41.1   s len @ 1024 xor abort" t41.1" ;
 : t41.2   s gosWofs@ 1024 xor abort" t41.2" ;
@@ -67,7 +68,7 @@ t41
 
 \ Saving blobs -- partial tail
 
-: s       srcbuf src !  0 dst !  150 len ! copy ;
+: s       srcbuf src !  0 dst !  150 len ! out ;
 : t42.0   s src @ srcbuf - 150 xor abort" t42.0" ;
 : t42.1   s len @ abort" t42.1" ;
 : t42.2   s gosWofs@ 150 xor abort" t42.2" ;
@@ -104,11 +105,65 @@ variable dst
 include ../lib/gos-space.fs
 
 : s       savbuf 10240 0 fill  datbuf src ! 150 dst ! 3072 len ! ;
-: i       savbuf 1024 dump ;
-: t43.0   s  keep i savbuf 10240 expected 10240 compare abort" t43.0" ;
+: t43.0   s  keep savbuf 10240 expected 10240 compare abort" t43.0" ;
 : t43.1   s  gosWofs@ >r keep gosWofs@ r> - 3072 xor abort" t43.1" ;
 : t43     t43.0 t43.1 ;
 t43
+
+\ Loading partial blobs
+
+done marker done
+
+create srcbuf
+  10240 allot
+  srcbuf 10240 0 fill
+  srcbuf 150 + 4090 $CD fill
+
+create ldbuf
+  4096 allot
+
+create expected
+  4096 allot
+  expected 4096 0 fill
+  expected 4090 $CD fill
+
+: update  ;
+: block   1024 * srcbuf + ;
+include ../lib/xspace.fs
+include ../lib/metadata.fs
+variable src
+variable dst
+variable len
+: gosWofs!  src ! ;
+: gosWofs@  src @ ;
+include ../lib/gos-space.fs
+
+: s       ldbuf 4096 0 fill  150 src !  ldbuf dst ! 1024 len !  inp ;
+: t44.0   s  src @ 1024 xor abort" t44.0" ;
+: t44.1   s  dst @ ldbuf - 874 xor abort" t44.1" ;
+: t44.2   s  len @ 150 xor abort" t44.2" ;
+: t44.3   s  ldbuf c@ $CD xor abort" t44.3" ;
+: t44     t44.0 t44.1 t44.2 t44.3 ;
+t44
+
+: s       ldbuf 4096 0 fill 0 src !  ldbuf dst !  2048 len !  inp ;
+: t45.0   s  src @ 1024 xor abort" t45.0" ;
+: t45.1   s  dst @ ldbuf - 1024 xor abort" t45.1" ;
+: t45.2   s  len @ 1024 xor abort" t45.2" ;
+: t45     t45.0 t45.1 t45.2 ;
+t45
+
+: s       ldbuf 4096 0 fill  0 src !  ldbuf dst !  204 len !  inp ;
+: t46.0   s  src @ 204 xor abort" t46.0" ;
+: t46.1   s  dst @ ldbuf - 204 xor abort" t46.1" ;
+: t46.2   s  len @ abort" t46.2" ;
+: t46     t46.0 t46.1 t46.2 ;
+t46
+
+: s       ldbuf 4096 0 fill  150 src !  ldbuf dst ! 4096 len ! retrieve ;
+: t47.0   s  expected 4096 ldbuf 4096 compare abort" t47.0" ;
+: t47     t47.0 ;
+t47
 
 done
 
