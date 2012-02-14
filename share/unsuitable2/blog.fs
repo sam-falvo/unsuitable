c276811f04dbbbf94a62d2d60347f12c5b8bf6ae
Samuel A. Falvo II <kc5tja@arrl.net>
2012-02-14 11:01:07 -0800

    Index page generation now works through expanding a template

--- share/unsuitable2/blog.fs
+++ share/unsuitable2/blog.fs
@@ -2,15 +2,21 @@ require lib/config.fs
 require lib/articles-db.fs
 require lib/article.fs
 require lib/views.fs
-require lib/templates.fs
+require lib/contents.fs
+
+variable src
+variable len
+
+require lib/template.fs
+
+variable out
 
 : items		begin dup while indexItem 1- repeat drop ;
 : n		#articles max-articles-on-index-page min ;
 : -empty	empty? if diagnostic r> drop then ;
 : content	-empty n items ;
-: footer 	S" theme/index-footer" contents ;
-: header 	S" theme/index-header" contents ;
-: mime 		." Content-Type: text/html" cr cr ;
-: indexPage 	mime header content footer ;
+: indexPage 	here src !  S" theme/indexPage" contents  here src @ - len !
+                here out !  expand  out @ here over - type ;
+
 indexPage bye
 
