f7128d6a6e395e720f2243d5574aa7df3ee304ed
Samuel A. Falvo II <kc5tja@arrl.net>
2012-02-14 19:11:43 -0800

    Form publish button causes form handler to be invoked (just repeats the blank form for now).  Field substitutions are currently blank.

--- share/unsuitable2/view/preview.fs
+++ share/unsuitable2/view/preview.fs
@@ -1,3 +1,11 @@
-: preview 	here src !  S" theme/PreviewPage" contents  src @ here over -  type ;
+: .blog-title 		." Sam's All New, New and Improved, New News News" ;
+: .article-error 	;
+: .article-title 	;
+: .article-abstract 	;
+: .article-body		;
+
+variable outp
+: preview 	here src !  S" theme/PreviewPage" contents  src @ here over - len !
+		here outp !  expand  here outp @ - type ;
 preview
 
