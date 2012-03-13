e88ba19101ad3da1ab9b67c4b0c739612cb9bc59
Samuel A. Falvo II <kc5tja@arrl.net>
2012-03-12 23:26:04 -0700

    Article table iterator

--- share/unsuitable2/test/articles-db.fs
+++ share/unsuitable2/test/articles-db.fs
@@ -30,5 +30,36 @@ include ../lib/articles-db.fs
 : t110          t110.0 ( t110.1 ( t110.2 ( t110.3 ( t110.4 ( t110.5 ( ) ;
 t110
 
+done marker done
+
+create buf	8 cells allot
+
+8 cells constant /artDbColumn
+: blocks ;
+buf constant articleIds
+: x@32 		@ $FFFFFFFF and ;
+: x 		2dup c!  1+ swap 8 rshift swap ;
+: x!32		x x x x 2drop ;
+: reset 	buf 8 cells $FF fill  1 8 buf + x!32 2 16 buf + x!32 4 20 buf + x!32 ;
+: artDbId@ 	;
+: artDbId!	drop ;
+
+1 constant titles
+1 constant abstracts
+1 constant bodies
+1 constant timestamps
+
+include ../lib/articles-db.fs
+
+variable v
+variable c
+: testproc	v @ or v !  1 c +! ;
+: s		0 v ! 0 c ! reset ['] testproc perform allArticles ;
+: t130.0	s c @ 3 xor abort" t130.0" ;
+: t130.1	s v @ 7 xor abort" t130.1" ;
+: t130 		t130.0 t130.1 ;
+t130
+
 done
+
 
